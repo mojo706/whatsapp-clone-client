9ae9fb31c77e6d25fba0008e481cf64880962203
mojo706 <xriseugene@gmail.com>
2020-08-06 14:51:07 +0300

    show message contents, add moment to format time better, prepare for some styling!

--- src/components/ChatListScreen/ChatsList.tsx
+++ src/components/ChatListScreen/ChatsList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { chats } from '../../db'
+import { chats } from '../../db';
+import moment from 'moment';
 
 const ChatsList: React.FC = () => (
   <div>
@@ -7,7 +8,13 @@ const ChatsList: React.FC = () => (
       {chats.map((chat) => (
         <li key={chat.id}>
           <img src={chat.picture} alt="Profile" />
-          <div>{chat.name}</div> 
+          <div>{chat.name}</div>
+          {chat.lastMessage && (
+            <React.Fragment>
+              <div>{chat.lastMessage.content}</div>
+              <div>{moment(chat.lastMessage.createdAt).format('HH:mm')}</div>
+            </React.Fragment>
+          )}
         </li>
       ))}
     </ul>
