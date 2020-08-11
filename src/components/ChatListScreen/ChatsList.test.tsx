e98a0249c20708771d463d7b7344baee3dfba695
mojo706 <xriseugene@gmail.com>
2020-08-11 16:46:08 +0300

    add test for the navigation to chat

--- src/components/ChatListScreen/ChatsList.test.tsx
+++ src/components/ChatListScreen/ChatsList.test.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { cleanup, render, waitFor } from '@testing-library/react';
+import {
+  cleanup,
+  render,
+  waitFor,
+  fireEvent,
+  screen,
+} from '@testing-library/react';
 import ChatsList from './ChatsList';
+import { createBrowserHistory } from 'history';
 
 describe('ChatsList', () => {
-  afterEach(cleanup);
-
-  it('renders fetched chats data', async () => {
+  beforeEach(() => {
+    const history = createBrowserHistory();
+    history.push(`/`)
     fetchMock.mockResponseOnce(
       JSON.stringify({
         data: {
@@ -24,19 +31,50 @@ describe('ChatsList', () => {
         },
       })
     );
+  })
 
-    {
-      const { container, getByTestId } = render(<ChatsList />);
+  afterEach(() => {
+    cleanup();
 
-      await waitFor(() => container);
+    delete window.location;
+    window = Object.create(window);
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: '/',
+      },
+      writable: true,
+    });
+  });
 
-      expect(getByTestId('name')).toHaveTextContent('Foo Bar');
-      expect(getByTestId('picture')).toHaveAttribute(
-        'src',
-        'https://localhost:4000/picture.jpg'
-      );
-      expect(getByTestId('content')).toHaveTextContent('Hello');
-      expect(getByTestId('date')).toHaveTextContent('00:00');
-    }
+  it('renders fetched chats data', async () => {
+    const history = createBrowserHistory();
+    const { container, getByTestId } = render(
+      <ChatsList history={history} />
+    );
+
+    await waitFor(() => screen.getByTestId('name'));
+
+    expect(getByTestId('name')).toHaveTextContent('Foo Bar');
+    expect(getByTestId('picture')).toHaveAttribute(
+      'src',
+      'https://localhost:4000/picture.jpg'
+    );
+    expect(getByTestId('content')).toHaveTextContent('Hello');
+    expect(getByTestId('date')).toHaveTextContent('00:00');
+  });
+
+  it('should navigate to the target chat room on chat item click', async () => {
+    const history = createBrowserHistory();
+    const { container, getByTestId } = render(
+      <ChatsList history={history} />
+    );
+
+    await waitFor(() => screen.getByTestId('chat'));
+
+    fireEvent.click(getByTestId('chat'));
+
+    await waitFor(() =>
+      expect(history.location.pathname).toEqual('/chats/1')
+    );
   });
 });
